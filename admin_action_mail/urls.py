9d1ea7572fe700be14b788b66563cd24d386214c
John Cronan <john@pbx.org>
2015-01-04 10:12:46 -0600

    no django.conf.urls.defaults

--- admin_action_mail/urls.py
+++ admin_action_mail/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('admin_action_mail.views',
         url(r'^mail_form/$','mail_form',name='admin_action_mail-form'),
